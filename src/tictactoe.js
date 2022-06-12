e3438f60f23c5d5e19362e2cb7a5a74b9de3313c
Jejoon Ryu <rjejoon@gmail.com>
2022-06-11 23:30:17 -0600

    Implement functions required for minimax

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -5,11 +5,16 @@ import Narration from './components/narration/narration';
 const TicTacToe = (function() {
   'use strict';
 
+  const MAX_PLAYER = 2;
   const BOARD_SIZE = 3;
   const bodyEle = document.querySelector("body");
 
   let player = 0;
+  // mode === 0: 2 player
+  // mode === 1: easy computer
+  // mode === 2: hard computer
   let mode = 0;
+  let computer = 1;   // default: computer is player === 1
 
   /**
    * Starts a new game of Tic Tac Toee.
@@ -35,12 +40,13 @@ const TicTacToe = (function() {
       Board.removeChildOnClickListener(row, col, boardEntryOnClickListener);
 
       if (hasPlayerWon()) {
-        Narration.setTextContent(`Congratulations ${player+1}! You won!`);
+        Narration.setTextContent(`Congratulations player ${player+1}! You won!`);
         Board.removeChildrenOnClickListerners(boardEntryOnClickListener);
       } else if (isFull()) {
         Narration.setTextContent("It's a draw.");
         Board.removeChildrenOnClickListerners(boardEntryOnClickListener);
       } else {
+        changePlayer();
         if (mode === 0) {
           twoPlayerGame();
         } else if (mode === 1) {
@@ -55,7 +61,6 @@ const TicTacToe = (function() {
   }
 
   function twoPlayerGame() { 
-    changePlayer();
     Narration.setTextContent(`It's player ${player+1}'s turn!`);
   }
 
@@ -79,17 +84,17 @@ const TicTacToe = (function() {
       Board.removeChildrenOnClickListerners(boardEntryOnClickListener);
     }
     else {
+      changePlayer();
       Board.enableOnClickForChildren(boardEntryOnClickListener, true);
       Narration.setTextContent(`It's player ${player+1}'s turn!`);
     }
   }
 
-  function hardComp() { 
-
-  }
-
   function getComputerEntry() {
-    return (player === 0) ? 'X' : 'O';
+    if (computer === 0) {
+      return (player === computer) ? 'O' : 'X';
+    }
+    return (player === computer) ? 'X' : 'O';
   }
 
   /**
@@ -111,6 +116,9 @@ const TicTacToe = (function() {
   }
 
   function getPlayerEntry() {
+    if (mode > 0) {
+      return (computer === 0) ? 'X' : 'O';
+    }
     return (player === 0) ? 'O' : 'X';
   }
 
@@ -176,6 +184,150 @@ const TicTacToe = (function() {
    */
   function isValidPlay(row, col) {
     return Board.getChild(row, col).textContent === '';
+  }
+
+  function hardComp() { 
+
+  }
+
+  function minimax(state, alpha=-Infinity, beta=Infinity) {
+    if (isTerminal(state)) {
+      return U(state);
+    }
+    M = state.isComputerTurn ? Infinity : -Infinity;
+    A(state).forEach(a => {
+      if (state.isComputerTurn) {
+        M = max(M, minimax(T(state, a), alpha, beta));
+        if (M >= beta) {
+          return M;
+        }
+        alpha = max(alpha, M);
+      } else {
+        M = min(M, minimax(T(state, a), alpha, beta));
+        if (M <= alpha) {
+          return M;
+        }
+        beta = min(beta, M);
+      }
+    });
+    return M;
+  }
+
+  function isComputerTurn() { 
+    return (computer === 0 && player === 0) || (computer === 1 && player === 1);
+  }
+
+  /**
+   * An utility function that returns a utility vector with one entry for each player given a terminal node.
+   * 
+   * For computer: 
+   *  +10 if victory
+   *  -10 if defeated
+   * 
+   * For player: 
+   *  -10 if victory
+   *  +10 if defeated
+   * 
+   *  0 if draw.
+   * 
+   * @param t_s terminal state 
+   */
+  function U(t_s) {
+    // get the last player's entry, not the current player
+    const entry = t_s.isComputerTurn ? getPlayerEntry() : getComputerEntnry();
+    if (stateCheckRows(t_s, entry) || stateCheckCols(t_s, entry) || stateCheckDiags(t_s, entry)) {
+      return t_s.isComputerTurn ? -10 : 10;
+    } 
+    return 0;
+  }
+
+  function currState() {
+    const s = {
+      board: [],
+      isComputerTurn: isComputerTurn(),
+    };
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      s.board.push([]);
+      Board.getChildrenOnRow(i).forEach(child => s.board[i].push(child.textContent));
+    }
+    return s;
+  }
+
+  function isTerminal(state) {
+    // get the last player's entry, not the current player
+    const entry = state.isComputerTurn ? getPlayerEntry() : getComputerEntnry();
+    if (stateCheckRows(state, entry) || stateCheckCols(state, entry) || stateCheckDiags(state, entry)) {
+      return true;
+    }
+
+    // check for draws
+    for (let i=0; i<BOARD_SIZE; i++) {
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        if (state[i][j] === '') {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  function stateCheckRows(state, entry) {
+    return state.board.some(row => row.every(cell => cell === entry));
+  }
+
+  function stateCheckCols(state, entry) {
+    let n_entry = 0;
+    for (let i=0; i<BOARD_SIZE; i++) {
+      for (let j=0; j<BOARD_SIZE; j++) {
+        if (state.board[j][i] === entry) {
+          n_entry++;
+        }
+      }
+      if (n_entry === BOARD_SIZE) {
+        return true;
+      }
+    } 
+    return false;
+  }
+
+  function stateCheckDiags(state, entry) {
+    const diags = [[], []];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      diags[0].push(state.board[i][i]);
+      diags[1].push(state.board[i][BOARD_SIZE-1-i]);
+    }
+
+    return diags.some(diag => diag.every(cell => cell === entry));
+  }
+
+  /**
+   * An action function that returns a set of possible 
+   * actions that the computer can take at a given state.
+   * 
+   * An action: [row, col]
+   */
+  function A(state) {
+    const actions = [];
+    for (let i=0; i<BOARD_SIZE; i++) {
+      for (let j=0; j<BOARD_SIZE; j++) {
+        if (state.board[i][j] === '') {
+          actions.push([i, j]);
+        }
+      }
+    }
+    return actions;
+  }
+
+  /**
+   * A transition function that returns a deep copy of the next state 
+   * after performing the given action with an appropriate entry.
+   */
+  function T(state, action) {
+    const copy_state = JSON.parse(JSON.stringify(state));
+    const [row, col] = action;
+    copy_state.board[row][col] = state.isComputerTurn ? getComputerEntry() : getPlayerEntry();
+    copy_state.isComputerTurn = !copy_state.isComputerTurn;
+    return copy_state;
   }
 
   return {
